c58c8982f13505991b0a2617e4d8d80643f18fc6
Bkhufa <khufa95@gmail.com>
2020-05-19 21:57:39 +0700

    random island generator

--- fp_game_cerdas/Assets/Scripts/TransformFollower.cs
+++ fp_game_cerdas/Assets/Scripts/TransformFollower.cs
@@ -28,7 +28,7 @@
  
              return;
          }
- 
+
          // compute position
          if(offsetPositionSpace == Space.Self)
          {
@@ -42,11 +42,43 @@
          // compute rotation
          if(lookAt)
          {
-             transform.LookAt(target);
+            // target.position = new Vector3 (target.transform.position.x, transform.position.y, target.transform.position.z);
+            transform.LookAt(target.position);
          }
          else
          {
-             transform.rotation = target.rotation;
+            transform.rotation = target.rotation;
          }
      }
+
+//     public Transform _lookTarget;
+
+//     // Cache the target we're supposed to look at.  
+//     public void LookAt(Transform target)
+//     {
+//     _lookTarget = target;
+//     }
+
+// // LateUpdate so it runs after any scripts/animation 
+// // that might move _lookTarget this frame.
+//     void LateUpdate()
+//         {
+//         if(_lookTarget != null)
+//         {
+//             Transform camera = Camera.main.transform;
+//             Vector3 toTarget = _lookTarget.position - camera.position;
+
+//             // This constructs a rotation looking in the direction of our target,
+//             Quaternion targetRotation = Quaternion.LookRotation(toTarget);
+
+//             // This blends the target rotation in gradually.
+//             // Keep sharpness between 0 and 1 - lower values are slower/softer.
+//             float sharpness = 0.1f;
+//             camera.rotation = Quaternion.Lerp(camera.rotation, targetRotation, sharpness);
+
+//             // This gives an "stretchy" damping where it moves fast when far
+//             // away and slows down as it gets closer. You can also use 
+//             // Quaternion.RotateTowards() to get a more consistent speed.
+//         }
+//     }
  }
