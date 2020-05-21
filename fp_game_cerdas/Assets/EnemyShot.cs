ad98f48155cf74dbae7429024885971e1df679b1
Ramadhan Ilham <ilham.17051@mhs.its.ac.id>
2020-05-21 10:00:46 +0700

    nembak

---
+++ fp_game_cerdas/Assets/EnemyShot.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyShot : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;              // Used to identify the different players.
+    public Rigidbody m_Shell;                   // Prefab of the shell.
+    private Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+    public Transform m_FireTransformright;           // A child of the tank where the shells are spawned.
+    public Transform m_FireTransformleft;           // A child of the tank where the shells are spawned.
+    public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
+    public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
+    public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
+    public int m_ShootDirection = 0;
+    public float m_MinLaunchForce = 30f;        // The force given to the shell if the fire button is not held.
+    public float m_MaxLaunchForce = 50f;        // The force given to the shell if the fire button is held for the max charge time.
+    public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
+    public float m_FireRate = 15f;
+
+    private string m_FireButton;                // The input axis that is used for launching shells.
+    private string m_AltFireButton;                // The input axis that is used for launching shells.
+    private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
+    private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
+    private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+    private float m_nextShoot = 0f;
+
+    public bool useInvoke;
+    public bool running;
+
+    //private void OnEnable()
+    //{
+        // When the tank is turned on, reset the launch force and the UI
+        //m_AimSlider = m_AimSliderLeft;
+        //m_CurrentLaunchForce = m_MinLaunchForce;
+        //m_AimSlider.value = m_MinLaunchForce;
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (useInvoke)
+             InvokeRepeating("TestInvoke", 0, 3);
+        else
+             StartCoroutine(TestCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_FireTransform = m_FireTransformleft;
+        
+    }
+
+    IEnumerator TestCoroutine()
+     {
+         running = true;
+         yield return new WaitForSeconds(5);
+         while (running)
+         {
+             yield return new WaitForSeconds(3);
+             Fire();
+         }
+     }
+
+
+    private void Fire ()
+    {
+        // Set the fired flag so only Fire is only called once.
+        m_Fired = true;
+
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance =
+            Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; ;
+
+        // Change the clip to the firing clip and play it.
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play ();
+
+        // Reset the launch force.  This is a precaution in case of missing button events.
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+}
